2b884eed763c9f66d36d6271f078223e5dc4fe6b
urvi <urvir1008@gmail.com>
2025-01-10 18:33:03 +0530

    first commit

---
+++ Android/ThriveDrive/app/src/main/java/com/example/thrivedrive/Activity/SplashActivity.java
@@ -0,0 +1,56 @@
+package com.example.thrivedrive.Activity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.bumptech.glide.Glide;
+import com.example.thrivedrive.R;
+import com.example.thrivedrive.Utils.ConstantData;
+
+public class SplashActivity extends AppCompatActivity {
+
+    SharedPreferences sp;
+    @SuppressLint("CustomSplashScreen")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        ImageView gifImageView = findViewById(R.id.gifImageView);
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.splashscreenapp)
+                .into(gifImageView);
+
+        sp = getSharedPreferences(ConstantData.SP_NAME,MODE_APPEND);
+        boolean isLogin = sp.getBoolean(ConstantData.SP_IS_LOGIN,false);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(isLogin){
+                    Intent intent=new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },3000);
+
+    }
+}
---
+++ Android/ThriveDrive/app/src/main/java/com/example/thrivedrive/Utils/ConstantData.java
@@ -0,0 +1,7 @@
+package com.example.thrivedrive.Utils;
+
+public class ConstantData {
+    public static final String SP_NAME = "MySharedPref";
+    public static final String SP_EMAIL = "email";
+    public static final String SP_IS_LOGIN = "isLogin";
+}
